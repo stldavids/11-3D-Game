a05f9188ab9ab6d2e82548ac9dcca8907065f505
Stephanie Davidson <rpookie98@gmail.com>
2019-11-27 16:02:58 -0500

    30 min into video

--- New Game Project/Scripts/World.gd
+++ New Game Project/Scripts/World.gd
@@ -1,6 +1,16 @@
 extends Spatial
 
+var score = 0
+
+
+
 func _unhandled_input(event):
 		if Input.is_action_pressed("quit"):
 			get_tree().quit()
 			
+			
+func increase_score(points):
+	score += points
+	
+func increase_size(size):
+	pass
