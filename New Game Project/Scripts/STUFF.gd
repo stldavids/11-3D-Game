0ba1559424c3473768a91bacef6ccaae256cefaf
Stephanie Davidson <rpookie98@gmail.com>
2019-11-27 17:50:05 -0500

    finished video

--- New Game Project/Scripts/STUFF.gd
+++ New Game Project/Scripts/STUFF.gd
@@ -6,7 +6,7 @@ export var increase_size = 0.2
 onready var World = get_node("/root/World")
 
 func _physics_process(delta):
-	if translation.y < -10:
+	if translation.y < -5:
 		World.increase_score(points)
 		World.increase_size(increase_size)
 		queue_free()
--- New Game Project/Scripts/World.gd
+++ New Game Project/Scripts/World.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 var score = 0
+var time_remaining = 120
+onready var Stuff = $Stuff
 
 
 
@@ -12,6 +14,18 @@ func _unhandled_input(event):
 func increase_score(points):
 	score += points
 	$HUD/Score.text = str(score)
+	if Stuff.get_child_count() == 1:
+		get_tree().change_scene("res://Scenes/YouWin.tscn")
 	
 func increase_size(size):
 	$Ground/Hole.increase_size(size)
+
+func _on_Countdown_timeout():
+	time_remaining -= 1
+	if time_remaining <= 0:
+		get_tree().change_scene("res://Scenes/GameOver.tscn")
+	else:
+		var m = time_remaining / 60
+		var s = time_remaining % 60
+		$HUD/Timer.text = str(m) + ":" + str(s)
+
