94de5d82d599f1ebc80ac2168f56e645f4b40c08
Stephanie Davidson <rpookie98@gmail.com>
2019-11-27 16:45:07 -0500

    40 min

--- New Game Project/Scripts/Hole.gd
+++ New Game Project/Scripts/Hole.gd
@@ -2,7 +2,7 @@ extends CSGCylinder
 
 var max_size = 1.5
 var min_size = 0.1
-var change_size = 0.01
+var change_size = 0.1
 var movement = 0.02
 
 func _ready():
@@ -10,11 +10,9 @@ func _ready():
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("increase_size"):
-		radius += change_size
-		radius = min(radius, max_size)
+		increase_size(change_size)
 	if Input.is_action_pressed("decrease_size"):
-		radius -= change_size
-		radius = max(radius, min_size)
+		decrease_size(change_size)
 	if Input.is_action_pressed("move_back"):
 		translation += Vector3(0,0,movement)
 	if Input.is_action_pressed("move_forward"):
@@ -23,3 +21,18 @@ func _physics_process(delta):
 		translation += Vector3(-movement,0,0)
 	if Input.is_action_pressed("move_right"):
 		translation += Vector3(movement,0,0)
+		
+func increase_size(size):
+	var target = radius + size
+	target = min(target,max_size)
+	if target != radius:
+		$Tween.interpolate_property(self,"radius", radius, target, 0.4, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+		$Tween.start()
+
+	
+func decrease_size(size):
+	var target = radius - size
+	target = max(target,min_size)
+	if target != radius:
+		$Tween.interpolate_property(self,"radius", radius, target, 0.4, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+		$Tween.start()
--- New Game Project/Scripts/World.gd
+++ New Game Project/Scripts/World.gd
@@ -11,6 +11,7 @@ func _unhandled_input(event):
 			
 func increase_score(points):
 	score += points
+	$HUD/Score.text = str(score)
 	
 func increase_size(size):
-	pass
+	$Ground/Hole.increase_size(size)
